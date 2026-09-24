19c81e85da67a72326a7206e6cd349cca364dd3c
fryanag <fryanagultom@gmail.com>
2026-09-24 22:40:10 +0700

    Praktikum

---
+++ Tugas3_Pemrograman_Visual/Tugas3_Pemrograman_Visual/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblSubtotal = New Label()
+        lblDiskon = New Label()
+        lblTotal = New Label()
+        chkMember = New CheckBox()
+        btnHitung = New Button()
+        txtBerat = New TextBox()
+        lblBerat = New Label()
+        lblST = New Label()
+        lblD = New Label()
+        lblT = New Label()
+        SuspendLayout()
+        ' 
+        ' lblSubtotal
+        ' 
+        lblSubtotal.AutoSize = True
+        lblSubtotal.Location = New Point(102, 342)
+        lblSubtotal.Name = "lblSubtotal"
+        lblSubtotal.Size = New Size(39, 20)
+        lblSubtotal.TabIndex = 0
+        lblSubtotal.Text = "Rp 0"
+        ' 
+        ' lblDiskon
+        ' 
+        lblDiskon.AutoSize = True
+        lblDiskon.Location = New Point(352, 342)
+        lblDiskon.Name = "lblDiskon"
+        lblDiskon.Size = New Size(29, 20)
+        lblDiskon.TabIndex = 1
+        lblDiskon.Text = "0%"
+        ' 
+        ' lblTotal
+        ' 
+        lblTotal.AutoSize = True
+        lblTotal.Location = New Point(598, 342)
+        lblTotal.Name = "lblTotal"
+        lblTotal.Size = New Size(39, 20)
+        lblTotal.TabIndex = 2
+        lblTotal.Text = "Rp 0"
+        ' 
+        ' chkMember
+        ' 
+        chkMember.AutoSize = True
+        chkMember.Location = New Point(260, 158)
+        chkMember.Name = "chkMember"
+        chkMember.Size = New Size(130, 24)
+        chkMember.TabIndex = 3
+        chkMember.Text = "Punya Member"
+        chkMember.UseVisualStyleBackColor = True
+        ' 
+        ' btnHitung
+        ' 
+        btnHitung.Location = New Point(339, 226)
+        btnHitung.Name = "btnHitung"
+        btnHitung.Size = New Size(94, 29)
+        btnHitung.TabIndex = 4
+        btnHitung.Text = "Hitung Total"
+        btnHitung.UseVisualStyleBackColor = True
+        ' 
+        ' txtBerat
+        ' 
+        txtBerat.Location = New Point(396, 86)
+        txtBerat.Name = "txtBerat"
+        txtBerat.Size = New Size(125, 27)
+        txtBerat.TabIndex = 5
+        ' 
+        ' lblBerat
+        ' 
+        lblBerat.AutoSize = True
+        lblBerat.Location = New Point(260, 89)
+        lblBerat.Name = "lblBerat"
+        lblBerat.Size = New Size(130, 20)
+        lblBerat.TabIndex = 6
+        lblBerat.Text = "Input Berat Kain   :"
+        ' 
+        ' lblST
+        ' 
+        lblST.AutoSize = True
+        lblST.Location = New Point(102, 300)
+        lblST.Name = "lblST"
+        lblST.Size = New Size(71, 20)
+        lblST.TabIndex = 7
+        lblST.Text = "Sub Total"
+        ' 
+        ' lblD
+        ' 
+        lblD.AutoSize = True
+        lblD.Location = New Point(352, 300)
+        lblD.Name = "lblD"
+        lblD.Size = New Size(54, 20)
+        lblD.TabIndex = 8
+        lblD.Text = "Diskon"
+        ' 
+        ' lblT
+        ' 
+        lblT.AutoSize = True
+        lblT.Location = New Point(596, 300)
+        lblT.Name = "lblT"
+        lblT.Size = New Size(42, 20)
+        lblT.TabIndex = 9
+        lblT.Text = "Total"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblT)
+        Controls.Add(lblD)
+        Controls.Add(lblST)
+        Controls.Add(lblBerat)
+        Controls.Add(txtBerat)
+        Controls.Add(btnHitung)
+        Controls.Add(chkMember)
+        Controls.Add(lblTotal)
+        Controls.Add(lblDiskon)
+        Controls.Add(lblSubtotal)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblDiskon As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents chkMember As CheckBox
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtBerat As TextBox
+    Friend WithEvents lblBerat As Label
+    Friend WithEvents lblST As Label
+    Friend WithEvents lblD As Label
+    Friend WithEvents lblT As Label
+
+End Class
---
+++ Tugas3_Pemrograman_Visual/Tugas3_Pemrograman_Visual/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        ' 1. Deklarasi Variabel & Input
+        Dim berat As Decimal
+        Dim hargaPerKg As Decimal = 7000D ' Harga Dasar Rp 7.000/kg
+        Dim subtotal As Decimal
+        Dim diskonPersen As Decimal = 0D
+        Dim jumlahDiskon As Decimal
+        Dim totalBayar As Decimal
+        Dim isMember As Boolean = chkMember.Checked
+
+        ' 2. Validasi Input
+        If Not Decimal.TryParse(txtBerat.Text, berat) OrElse berat <= 0 Then
+            MessageBox.Show("Masukkan jumlah berat laundry yang valid dan lebih dari 0!", "Error Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' 3. Hitung Subtotal
+        subtotal = berat * hargaPerKg
+
+        ' 4. Aturan Percabangan Diskon
+        ' Aturan A: Diskon Berat (Kuantitas)
+        If berat >= 5 Then
+            diskonPersen = diskonPersen + 0.1D ' Diskon 10% jika berat >= 5 kg
+        End If
+
+        ' Aturan B: Diskon Member Tambahan (Menggunakan Operator And)
+        If isMember And subtotal >= 30000D Then
+            diskonPersen = diskonPersen + 0.05D ' Tambahan diskon 5% untuk member dengan belanja min Rp 30.000
+        End If
+
+        ' Aturan C: Pembatasan Diskon Maksimal (Nested/Batas Diskon)
+        If diskonPersen > 0.15D Then
+            diskonPersen = 0.15D ' Maksimal diskon 15%
+        End If
+
+        ' 5. Kalkulasi Output
+        jumlahDiskon = subtotal * diskonPersen
+        totalBayar = subtotal - jumlahDiskon
+
+        ' 6. Tampilkan Hasil ke Label
+        lblSubtotal.Text = "Rp " & subtotal.ToString("N0")
+        lblDiskon.Text = (diskonPersen * 100).ToString("G") & "% (Rp " & jumlahDiskon.ToString("N0") & ")"
+        lblTotal.Text = "Rp " & totalBayar.ToString("N0")
+    End Sub
+End Class
